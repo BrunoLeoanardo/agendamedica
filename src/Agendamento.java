73e54591ba5ad65d2f3d630abb0de8094c01efa0
Ana Nínive <ananiniveribeiro@gmail.com>
2025-12-04 12:20:46 -0300

    Troquei idade pelo email

--- src/Agendamento.java
+++ src/Agendamento.java
@@ -31,7 +31,7 @@ public class Agendamento {
             if(pacientes.get(i).getCpf(). equals(cpf)){
                 System.out.println(pacientes.get(i).getNome());
                 System.out.println(pacientes.get(i).getTelefone());
-                System.out.println(pacientes.get(i).getIdade());
+                System.out.println(pacientes.get(i).getEmail());
                 i=pacientes.size();
             }
         }
@@ -64,7 +64,7 @@ public class Agendamento {
         System.out.println("Data do atendimento:"+data+", Médico(a):"+medico.getNome()+", "+medico.getEspecialidade());
         for (int i=0; i<pacientes.size(); i++){
             Paciente p  = pacientes.get(i);
-            System.out.println(i+1+","+p.getNome()+", "+p.getCpf()+", "+p.getIdade());
+            System.out.println(i+1+","+p.getNome()+", "+p.getCpf()+", "+p.getEmail());
         }
     }
 }
